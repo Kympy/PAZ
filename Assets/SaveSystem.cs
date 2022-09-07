024bb716de21b576f04be7aba1ebe22bd465a515
Parrot_Yong <65384983+Kympy@users.noreply.github.com>
2022-09-07 19:04:07 +0900

    Update SaveSystem.cs

--- Assets/SaveSystem.cs
+++ Assets/SaveSystem.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 public static class SaveSystem
 {
-	public struct SaveData
+	public struct SaveData // Data which i will save
     {
 		public float lastPosX;
 		public float lastPosY;
@@ -21,12 +21,12 @@ public static class SaveSystem
 
 	public static void Save(SaveData saveData, string saveFileName)
 	{
-		if (!Directory.Exists(SavePath))
+		if (Directory.Exists(SavePath) == false) // There's no directory
 		{
 			Directory.CreateDirectory(SavePath);
 		}
 
-		string saveJson = JsonUtility.ToJson(saveData);
+		string saveJson = JsonUtility.ToJson(saveData); // Text to Json
 
 		string saveFilePath = SavePath + saveFileName + ".json";
 		File.WriteAllText(saveFilePath, saveJson);
@@ -37,13 +37,13 @@ public static class SaveSystem
 	{
 		string saveFilePath = SavePath + saveFileName + ".json";
 
-		if (!File.Exists(saveFilePath))
+		if (File.Exists(saveFilePath) == false) // There's no file
 		{
 			Debug.LogError("No such saveFile exists");
 			SaveData? save = null;
 			return save;
 		}
-
+		// Json to text
 		string saveFile = File.ReadAllText(saveFilePath);
 		SaveData saveData = JsonUtility.FromJson<SaveData>(saveFile);
 		return saveData;
